0ae3b635c49b324db3fad5e2f3ffb4e811f6cc1b
Kayhlacr <kayhla@student.dev-island.org>
2024-03-07 20:28:47 -1000

    added styles and add navigation bar and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-// import { useState } from "react";
-
 import "./App.css";
 import StudentListContainer from "./routes/StudentListContainer";
 import CatListContainer from "./routes/CatsListContainer";
 import DogListContainer from "./routes/DogsListContainer";
-import { Routes, Route } from "react-router-dom";
+import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+  const pageRoutes = ["/", "/cats", "/dogs"];
 
   return (
     <>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/cats">Cats</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dogs">Dogs</NavLink>
+          </li>
+        </ul>
+        <button
+          onClick={() => {
+            const randomPageIndex = Math.floor(
+              Math.random() * pageRoutes.length
+            );
+            navigate(pageRoutes[randomPageIndex]);
+          }}
+        >
+          Random Page
+        </button>
+      </nav>
+
       <Routes>
         <Route path="/" element={<StudentListContainer />} />
         <Route path="/cats" element={<CatListContainer />} />
